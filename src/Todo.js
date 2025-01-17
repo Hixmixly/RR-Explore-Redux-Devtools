9adfb4cd6cba81ca10aa8f170074094e4cab1036
Hixmixly <152249445+Hixmixly@users.noreply.github.com>
2025-01-17 15:47:25 -0800

    updated todo.js to with the functionality to clear seperate list items by clicking on them

--- src/Todo.js
+++ src/Todo.js
@@ -1,31 +1,44 @@
-import React, { useState } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { addTodo, removeOne, clearTodo } from './features/todoSlice'
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { addTodo, removeOne, clearTodo } from './features/todoSlice';
 
 function Todo() {
-    const items = useSelector((state) => state.todos.items)
-    const dispatch = useDispatch()
-    const [input, setInput] = useState('')
+  const items = useSelector((state) => state.todos.items); // Accessing to-dos from Redux store
+  const dispatch = useDispatch();
+  const [input, setInput] = useState('');
 
-    const renderItems = items.map((item, index) => <li key={index} onClick={() => dispatch(removeOne(index))}>{item}</li>)
-
-    const submitForm = (e) => {
-        e.preventDefault()
-        dispatch(addTodo(input))
+  // Function to handle adding a to-do
+  const submitForm = (e) => {
+    e.preventDefault();
+    if (input.trim()) { // Prevent empty to-dos
+      dispatch(addTodo(input));
+      setInput(''); // Clear input after submission
     }
+  };
 
-    return (
-        <div>
-            <form onSubmit={(e) => submitForm(e)}>
-                <input type="text" onChange={(e) => setInput(e.target.value)} />
-                <button type="submit">Submit</button>
-            </form>
-            <ul>
-                {renderItems}
-            </ul>
-            <button onClick={() => dispatch(clearTodo())}>Clear</button>
-        </div>
-    )
+  // Render to-do items with remove functionality
+  const renderItems = items.map((item, index) => (
+    <li key={index} onClick={() => dispatch(removeOne(index))}>
+      {item}
+    </li>
+  ));
+
+  return (
+    <div>
+      <h1>To-Do List</h1>
+      <form onSubmit={submitForm}>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Enter a to-do"
+        />
+        <button type="submit">Add</button>
+      </form>
+      <ul>{renderItems}</ul>
+      <button onClick={() => dispatch(clearTodo())}>Clear All</button>
+    </div>
+  );
 }
 
-export default Todo
+export default Todo;
